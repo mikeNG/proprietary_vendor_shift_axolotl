60bc0f82f48ded005fd5471248df0bb5d8b12c70
Michael Bestas <mkbestas@lineageos.org>
2023-12-13 18:50:44 +0200

    axolotl: Import firmware from SHIFT6MQ.SOS.3.10.G.20231025-RELEASE
    Change-Id: I7f8c889612c9d1e8789263a4780ca9cce29c131f

--- Android.mk
+++ Android.mk
@@ -6,4 +6,21 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),axolotl)
 
+$(call add-radio-file-sha1-checked,radio/ImageFv.img,743fca7232dee508c650af4089bdcacdc0cbcabe)
+$(call add-radio-file-sha1-checked,radio/abl.img,87261da98a6cf750e8df99a5727daffd4584f9fd)
+$(call add-radio-file-sha1-checked,radio/aop.img,7de175c9260a0f7914a4c832eb8998aa53f70e9f)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,36cdfa2a90989c1eb0dd15743b28a081d74553fb)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,b070a992cfd2932203ff005a433eabc7efbf35db)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,06156196cbcc47107f8221c0f377184f1dd34ebd)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,8244078871dc050f56866787e7d033f6115da45b)
+$(call add-radio-file-sha1-checked,radio/dsp.img,755b1c06719bfcae87ed9fdf4ecc69d001ce6232)
+$(call add-radio-file-sha1-checked,radio/hyp.img,5bd53f4bc48b57bb118a889584900ed5bbb4fff9)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,fd0be46190f812fcbd677d6e0ecd391bf0d2d334)
+$(call add-radio-file-sha1-checked,radio/modem.img,897d1d3562e8b8c24abac37798588c1e42b9a2fc)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,5be9b772b7ad9b021ad5c7e832d5b6f5d0db8d93)
+$(call add-radio-file-sha1-checked,radio/storsec.img,aa532e9f1b5bc8ab5fe2e6ee8a42a775212151fb)
+$(call add-radio-file-sha1-checked,radio/tz.img,f5f048a8d118bcbe50d2c2e8831d7791300a6595)
+$(call add-radio-file-sha1-checked,radio/xbl.img,06efd08572f985ab09b861279f318b825b38e64a)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,4fbef453c9b95e15f48b2003d94e7b5763a96b2b)
+
 endif
